235bbabaeb6e415e253a897e92ff18df0cf476ad
kqiita <kqiita1040@gmail.com>
2025-08-25 19:28:03 +0900

    feat: add _components/globalnav/Footer  chore: change tsconfig.json @/components components -> _components

---
+++ src/_components/globalnav/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+export function Footer(){
+    return (
+        <div>
+            <p>&copy; 2025 kqiita</p>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import {Footer} from "@/components/globalnav/Footer";
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <div>
+    <h1>Hello, Next.js!</h1>
+    <Footer />
+    </div>
+  );
 }
