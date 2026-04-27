821f6973dc4b2bbfddaeb955c3017c06eee43e60
kqiita <kqiita1040@gmail.com>
2026-04-27 20:09:38 +0900

    test: change test.md

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_C5NLF from "../../../public/img/syahuneko-ver1-r225g234b205.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftest.md";
+export default new Map([["../../../public/img/syahuneko-ver1-r225g234b205.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftest.md", __ASTRO_IMAGE_IMPORT_C5NLF]]);
+		
--- src/content/blog/test.md
+++ src/content/blog/test.md
@@ -6,3 +6,15 @@ pubDate: "2026-04-17"
 
 # 見出し
 本文
+## **太字**
+## _斜体_
+~~取消線~~
+
+[リンク](/)
+
+```js
+console.log("hello")
+```
+
+![hnkk.me](../../../public/img/syahuneko-ver1-r225g234b205.png)
+:face:
