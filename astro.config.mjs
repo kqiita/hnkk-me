12043daad0ad3afcb8ae871e7533762024d06d64
kqiita <kqiita1040@gmail.com>
2026-05-26 02:08:17 +0900

    chore: add cfworkers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from "astro/config";
+import cloudflare from '@astrojs/cloudflare';
+
 import remarkToc from "remark-toc";
 import rehypeSlug from "rehype-slug";
 import remarkDirective from "remark-directive";
@@ -9,6 +11,7 @@ import directiveHandler from "./src/plugins/custom/directiveHandler";
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: cloudflare(),
     markdown: {
         remarkPlugins: [[remarkToc, {heading:"目次"},], remarkDirective,directiveHandler],
         rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
